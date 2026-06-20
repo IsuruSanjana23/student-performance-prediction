928f3b2f29fcc514d71bc0f3cc127d711a2922da
IsuruSanjana23 <isuru.sanjana23@gmail.com>
2026-06-21 04:09:39 +0530

    modify main.py

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import pandas as pd
 
-df = pd.read_csv('D:\Projects\student-performance-prediction\data\data.csv')
-
-"""
-print("First 5 rows:")
-print(df.head())
-
-print("\nShape:")
-print(df.shape)
-
-print("\nColumn names:")
-print(df.columns)
-
-print("\nMissing values:")
-print(df.isnull().sum())
-"""
 
 from src.preprocess import load_and_preprocess_data
 from src.train import train_model
@@ -22,14 +6,14 @@ from src.evaluate import evaluate_model
 from src.predict import predict_grade , predict_grade_input_data
 from src.visualize import plot_predictions
 
-X_train,X_test,y_train,y_test = load_and_preprocess_data()
+#X_train,X_test,y_train,y_test = load_and_preprocess_data()
 
 #print("X_train shape:", X_train.shape)
 #print("X_test shape:", X_test.shape)
 
-model = train_model(X_train, X_test, y_train, y_test)
+#model = train_model(X_train, X_test, y_train, y_test)
 
-train_r2 , test_r2 , predictions = evaluate_model(model, X_train, X_test, y_train, y_test)
+#train_r2 , test_r2 , predictions = evaluate_model(model, X_train, X_test, y_train, y_test)
 
 
 #print(f"Train R2: {train_r2:.2f}")
