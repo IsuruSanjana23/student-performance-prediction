7bc1164747237600799d5bb744aaebc2c0091425
IsuruSanjana23 <isuru.sanjana23@gmail.com>
2026-06-21 13:51:50 +0530

    Fixed Cors

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://student-performance-prediction-p14b1mgzl.vercel.app"
+        "https://student-performance-prediction-production-a3d5.up.railway.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
